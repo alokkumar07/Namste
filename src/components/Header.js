2e09be7c30157df4c60180c678b6dbe513f790cb
alokkumar07 <aloknith07@gmail.com>
2023-01-22 00:48:14 +0530

    chap-05

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+const Title = () => (
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvTiYvL2R6PNlChsx7LeLbHF23-Fn9MXctMg&usqp=CAU"
+      />
+    </a>
+  );
+  
+ const Header = () => {
+    return (
+      <div className="header">
+        <Title />
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li> 
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+ export default Header;
